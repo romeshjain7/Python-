e719e208f958f8f46a016798bfde6f6047d765b5
Romesh Jain <romeshjain73@gmail.com>
2025-04-08 16:18:17 +0530

    add example

--- list compression/example9.py
+++ list compression/example9.py
@@ -1,11 +1,32 @@
 m=int(input('Enter the number of row'))
 n=int(input('Enter col no'))
 
+
+a=[[int(input(f'Enter the value for a[{i}][{j}]')) for j in range(m)] for i in range(n)]
+b=[[int(input(f'Enter the value for b[{i}][{j}]')) for j in range(m)] for i in range(n)]
+c=[[(a[i][j]+b[i][j]) for j in range(m)] for i in range(n)]
+
+
 print()
-out=[]
 for i in range(m):
-    inner_list=[]
+    print('|',end=" ")
     for j in range(n):
-       inner_list.append(int(input(f'Enter the value for matrix- out[{i}][{j}])')))
-    out.append(inner_list)
-print(out)
+       print(a[i][j],end=" ")
+    print("|")
+
+print('*'*10)
+
+for i in range(m):
+    print('|',end=" ")
+    for j in range(n):
+       print(b[i][j],end=" ")
+    print("|")
+
+print('*'*10)
+
+
+for i in range(m):
+    print('|',end=" ")
+    for j in range(n):
+       print(c[i][j],end=" ")
+    print("|")
